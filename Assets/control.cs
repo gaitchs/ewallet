6554cc9fae1388bf87abaeaf6862157c48d55883
gaitchs <gaitchs@gmail.com>
2017-11-02 12:42:27 +0530

    jfdsklfjdskl

---
+++ Assets/control.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+
+
+	public void checkbalance(string str)
+	{
+		Debug.Log ("Your wallet adress is " + str);
+       
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ewallet
+# unitygamewallet
